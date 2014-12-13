115c777f9065b9136ec33a0b1483d48c48af514b
kshenx3 <kshenx3@users.noreply.github.com>
2014-12-12 23:30:46 -0500

    Update HomesForAll.java
    comments explaining thought process on how to implement hash table merr

--- finalProject_msit_kshen2/HomesForAll.java
+++ finalProject_msit_kshen2/HomesForAll.java
@@ -13,28 +13,44 @@
  * 
  */
 
+import java.util.*;
+import java.io.*;
 
 public class HomesForAll {
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   //-------------------------------Instance Variables
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  
-  
-  
+  private LinkedList searchRes, database;
+  private File output;
+  private int count;
+  private Hashtable<String, LinkedList> sortingHat; /*kekeke*/
   
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   //------------------------------Constructor Methods
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // HomesForAll():
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  /**
+   * Creates an instance of HomesForAll class when there is no input database to refer to.
+   * Will create a new database from scratch. 
+   */ 
   public HomesForAll() {
+    count = 0;
+    sortingHat = new Hashtable<String, LinkedList>();
+    database = new LinkedList();
+  }
+  
+  
+  /**
+   * Creates a HomesForAll object with an input file that acts as the loaded database reference.
+   * 
+   * @param fileName Name of the database reference as a String
+   */
+  public HomesForAll(String fileName) {
     
     
   }
   
-  
+
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   //---------------------------------Instance Methods
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -42,4 +58,47 @@ public class HomesForAll {
   
   
   
+  /**
+   * Returns a String representation of the HomesForAll class.
+   * 
+   * @return Returns HomesForAll.
+   */
+  public String toString() {
+    return "";
+  }
+  
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //--------------------------------------Main Method
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  public static void main (String[] args) {
+    HomesForAll test = new HomesForAll();
+    
+    test.sortingHat.put("Meow", test.database);
+    test.database.push("Meow");
+    test.database.push("Argh");
+    test.sortingHat.put("Argh", test.database);
+    
+    System.out.println(test.sortingHat.get("Meow"));
+    
+    //what this tells me is... 
+    /* i need to individually go through the entire list and check for the characteristic
+     * add that to the linked list
+     * the hashtable only allows for quick searching & returning the linked list, 
+     * does not actually sort the items into the linked list...
+     * 
+     * so each find____ method should first call upon a helper method that 
+     * both initializes and then sorts the database items into linkedlists that 
+     * the hash table then will sort into
+     * then at the end, the find____ method will return the value of the key input... 
+     * the "search"
+     * 
+     */
+    
+    //also need to find a way to distinguish between Homes & Apartments when going through a list of them
+    //** my soln: create a method in both classes that return a String "type" that is final
+    //** so that we can check if .equals? 
+    //** OR 
+    //** we can assign a final integer that is symbolic of either home or apartment
+    
+  }
 }
