c9fbd03fe8be4a8d379d5ff2d36a234976143cf4
kshenx3 <kshenx3@users.noreply.github.com>
2014-12-14 16:38:07 -0500

    Updated AddPanel
    basically does the same thing from a few nights ago but this time directly in the class instead of calling from somewhere else

--- finalProject_msit_kshen2/AddPanel.java
+++ finalProject_msit_kshen2/AddPanel.java
@@ -14,20 +14,60 @@ import java.awt.Color;
 import javax.swing.*;
 
 
-public class AddPanel extends JPanel {
+public class AddPanel extends JPanel implements ItemListener {
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //-------------------------------Instance Variables
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  private JPanel cards; 
   
+  private final static String HOMEPANEL = "Add Home";
+  private final static String APTPANEL = "Add Apartment";
+  
+  
+
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //------------------------------Constructor Methods
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   public AddPanel() {
-    
-    
-    
-    
+    //bc idk
   }
   
   
   public AddPanel(HomesForAll input) {
     
+    JPanel comboBoxPane = new JPanel(); //use FlowLayout
+    String comboBoxItems[] = { HOMEPANEL, APTPANEL };
+    JComboBox cb = new JComboBox(comboBoxItems);
+    cb.setEditable(false);
+    cb.addItemListener(this);
+    comboBoxPane.add(cb);
+    
+    //Create the "cards".
+    JPanel card1 = new JPanel();
+    card1.add(new JButton("Button 1"));
+    card1.add(new JButton("Button 2"));
+    card1.add(new JButton("Button 3"));
+    
+    JPanel card2 = new JPanel();
+    card2.add(new JTextField("TextField", 20));
+    
+    //Create the panel that contains the "cards".
+    cards = new JPanel(new CardLayout());
+    cards.add(card1, HOMEPANEL);
+    cards.add(card2, APTPANEL);
+    
+    
+    add(comboBoxPane, BorderLayout.PAGE_START);
+    add(cards, BorderLayout.CENTER);
     
   }
   
+  
+  
+  public void itemStateChanged(ItemEvent evt) {
+        CardLayout cl = (CardLayout)(cards.getLayout());
+        cl.show(cards, (String)evt.getItem());
+    }
+
   
 }
